d75ea74a20490692d5f4520d167f0c633f94ce15
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-05-19 17:52:41 -0300

    feat: not found page

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.home import home
 from routes.products import products
 from routes.admin import admin
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def not_found(e):
+  return render_template('404.html')
+
 app.register_blueprint(home)
-
-app.register_blueprint(admin, url_prefix= "/admin")
-
-app.register_blueprint(products)
+app.register_blueprint(admin, url_prefix="/admin")
+app.register_blueprint(products, url_prefix="/products")
